e8372e1fb307538040cc90848477277bf3abe4f9
unknown <utkristashestha984@gmail.com>
2024-01-05 21:10:45 +0545

    first week

---
+++ Portfolio/Week 1/Q.3.py
@@ -0,0 +1,10 @@
+'''Over the summer, temperatures in Yorkshire reached 38.4C. Write a program that
+converts this value in Celsius to the equivalent temperature in Fahrenheit, and then
+displays both.'''
+
+Value_in_Celsius = 38.4
+
+Value_in_Fahrenheit =  (9/5 * Value_in_Celsius) + 32
+
+print("Temperatures of Yorkshire in Celsius: ",Value_in_Celsius)
+print("Temperatures of Yorkshire in Fahrenheit: ",Value_in_Fahrenheit)
